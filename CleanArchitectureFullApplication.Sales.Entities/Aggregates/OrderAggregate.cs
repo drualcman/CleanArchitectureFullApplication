c44a760de61a51f449357dbfd59542b90a62bd7a
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-12 09:59:06 +0800

    cambiar orderdetail like valueobjet

--- CleanArchitectureFullApplication.Sales.Entities/Aggregates/OrderAggregate.cs
+++ CleanArchitectureFullApplication.Sales.Entities/Aggregates/OrderAggregate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitectureFullApplication.Sales.Entities.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- CleanArchitectureFullApplication.Sales.Entities/ValueObjects/OrderDetail.cs
+++ CleanArchitectureFullApplication.Sales.Entities/ValueObjects/OrderDetail.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CleanArchitectureFullApplication.Sales.Entities
+namespace CleanArchitectureFullApplication.Sales.Entities.ValueObjects
 {
     public class OrderDetail
     {
-        public int OrderId { get; set; }
         public int ProductId { get; set; }
         public decimal UnitPrice { get; set; }
         public short Quantity { get; set; }
