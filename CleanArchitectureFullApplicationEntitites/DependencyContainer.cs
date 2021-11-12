b6b883e640e69e4402dc9dc5adbfedd013a4b258
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-12 11:29:28 +0800

    agregada la dependencia

---
+++ CleanArchitectureFullApplicationEntitites/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitectureFullApplication.Main.Events;
+using CleanArchitectureFullApplication.Main.Validators;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureFullApplication.Main
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddMainServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IEventHub<>), typeof(EventHub<>));
+            services.AddSingleton(typeof(IValidatorService<>), typeof(ValidatorService<>));
+            return services;
+        }
+    }
+}
