e5c1e342facd93d40d1078786208670187f73fa7
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-17 11:27:17 +0800

    preparando la base de datos

---
+++ CleanArchitectureFullApplication.EFCore.DataContext/CleanArchitectureSalesContext.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitectureFullApplication.Main.Entities;
+using CleanArchitectureFullApplication.Sales.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CleanArchitectureFullApplication.EFCore.DataContext
+{
+    public class CleanArchitectureSalesContext : DbContext
+    {
+        public CleanArchitectureSalesContext(DbContextOptions<CleanArchitectureSalesContext> options) 
+            : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            //configurar las relaciones y propiedades de las tablas
+            modelBuilder.Entity<Order>()
+                .Property(o=>o.CustomerId).IsRequired().HasMaxLength(5).IsFixedLength();
+            modelBuilder.Entity<Order>()
+                .Property(o=>o.ShipAddress).IsRequired().HasMaxLength(60);
+            modelBuilder.Entity<Order>()
+                .Property(o=>o.ShipCity).HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCountry).HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipPostalCode).HasMaxLength(10);
+
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new { od.OrderId, od.ProductId });
+        }
+    }
+}
---
+++ CleanArchitectureFullApplication.EFCore.DataContext/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureFullApplication.Sales.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureFullApplication.EFCore.DataContext
+{
+    public class OrderDetail : Sales.Entities.ValueObjects.OrderDetail
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
