ff2a7c77b43c877339111c3210c69094e5e8679a
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-18 10:45:29 +0800

    agregar mail service

---
+++ CleanArchitectureFullApplication.Mail/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureFullApplication.Main.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureFullApplication.Mail
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddMailService(this IServiceCollection services)
+        {
+            services.AddTransient<IMailService, MailService>();
+            return services;
+        }
+    }
+}
---
+++ CleanArchitectureFullApplication.Mail/MailService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitectureFullApplication.Main.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureFullApplication.Mail
+{
+    public class MailService : IMailService
+    {
+        readonly IApplicationStatusLogger Logger;
+        public MailService(IApplicationStatusLogger logger) => Logger = logger;
+
+        public ValueTask Send(string message)
+        {
+            Logger.Log($"*** Mail service: {message}");
+            Logger.Log($"*** Mail service: Servidor de correo no configurado.");
+            return ValueTask.CompletedTask;
+        }
+    }
+}
