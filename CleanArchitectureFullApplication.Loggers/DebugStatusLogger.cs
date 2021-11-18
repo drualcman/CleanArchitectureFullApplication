f30864125dfa2e01095155fd6f1657cf2b402ed9
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-18 10:51:38 +0800

    aggregar loggers

---
+++ CleanArchitectureFullApplication.Loggers/DebugStatusLogger.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureFullApplication.Main.Interfaces;
+using System;
+using System.Diagnostics;
+
+namespace CleanArchitectureFullApplication.Loggers
+{
+    public class DebugStatusLogger : IApplicationStatusLogger
+    {
+        public void Log(string message)
+        {
+            Debug.WriteLine($"*** Application Log: {message}");
+        }
+    }
+}
---
+++ CleanArchitectureFullApplication.Loggers/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureFullApplication.Main.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureFullApplication.Loggers
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddLoggers(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationStatusLogger, DebugStatusLogger>();
+            return services;
+        }
+    }
+}
