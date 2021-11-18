650b5fcf2ee60ff1ab3e896c84b939db491a51d1
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-18 10:22:10 +0800

    agregar el Inversion Of Control para agregar los repositorios de una vez

---
+++ CleanArchitectureFullApplication.Repositories.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitectureFullApplication.EfCore.Repositories;
+using CleanArchitectureFullApplication.EFCore.DataContext;
+using CleanArchitectureFullApplication.Main.Interfaces;
+using CleanArchitectureFullApplication.Sales.UseCases.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureFullApplication.Repositories.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services,
+            IConfiguration configuration, string connectionName)
+        {
+            services.AddDbContext<CleanArchitectureSalesContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(connectionName)));
+            services.AddScoped<ILogWritableRepository, LogWritableRepository>();
+            services.AddScoped<IOrderWritableRepository, OrderWritableRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
